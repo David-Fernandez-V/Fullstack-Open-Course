27c2fb068d63291eb3a2c33f78f3b0179678a523
David Fernández <david.fernandez.v04@gmail.com>
2025-11-29 18:38:43 -0600

    Exercise 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -25,14 +25,6 @@ const Statistics = ({good, neutral, bad}) => {
   )
 }
 
-const Menu = () =>{
-  return(
-    <>
-
-    </>
-  )
-}
-
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
