e3de0bea43ed86dc6b6d0f6f3444d339e8a49d47
David-Fernandez-V <david.fernandez.v04@gmail.com>
2025-12-03 14:32:04 -0600

    Exercise 2.1

--- part2/courseinfo/src/main.jsx
+++ part2/courseinfo/src/main.jsx
@@ -1,8 +1,4 @@
-import ReactDOM from "react-dom/client"
-import App2 from './App2.jsx'
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App2/>
-)
-
-
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
