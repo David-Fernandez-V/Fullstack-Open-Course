a916c4fad381706ed8fb8793791c662c5e4d9556
David-Fernandez-V <david.fernandez.v04@gmail.com>
2025-12-03 15:50:29 -0600

    Exercises 2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,43 +1,6 @@
+import Course from "./components/Course";
+
 const Header = ({ header }) => <h1>{header}</h1>;
-
-const CourseHeader = ({ course }) => <h2>{course.name}</h2>;
-
-const Part = ({ part }) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-);
-
-const Content = ({ parts }) => (
-  <div>
-    {parts.map((part) => (
-      <Part key={part.id} part={part} />
-    ))}
-  </div>
-);
-
-const Total = ({ parts }) => {
-  let total = parts.reduce((sum, part) => {
-    sum += part.exercises;
-    return sum;
-  }, 0);
-
-  return (
-    <p>
-      <strong>Total of {total} exercises</strong>
-    </p>
-  );
-};
-
-function Course({ course }) {
-  return (
-    <div>
-      <CourseHeader course={course} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
-}
 
 const App = () => {
   const courses = [
