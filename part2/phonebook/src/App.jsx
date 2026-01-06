636ef557e4138eb8fa396e6cf152c4aad472345a
David-Fernandez-V <david.fernandez.v04@gmail.com>
2026-01-06 13:52:13 -0600

    Exercise 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,26 +1,41 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "3348965722" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const addPerson = (e) => {
     e.preventDefault();
 
+    if (persons.find((p) => p.name === newName)) {
+      alert(`${newName} is already added to the Phonebook`);
+      return;
+    }
+
+    if (persons.find((p) => p.number === newNumber)) {
+      alert(`${newNumber} is already added to the Phonebook`);
+      return;
+    }
+
     const person = {
       name: newName,
+      number: newNumber,
     };
 
-    if (persons.find((p) => p.name === person.name)) {
-      alert(`${newName} is already added to the Phonebook`);
-    } else {
-      setPersons(persons.concat(person));
-      setNewName("");
-    }
+    setPersons(persons.concat(person));
+    setNewName("");
+    setNewNumber("");
   };
 
-  const handlePersonChange = (e) => {
+  const handleNameChange = (e) => {
     setNewName(e.target.value);
+  };
+
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value);
   };
 
   return (
@@ -28,7 +43,10 @@ const App = () => {
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input value={newName} onChange={handlePersonChange} />
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button onClick={addPerson}>add</button>
@@ -36,7 +54,9 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {persons.map((p) => (
-        <p key={p.name}>{p.name}</p>
+        <p key={p.name}>
+          {p.name}: {p.number}
+        </p>
       ))}
     </div>
   );
