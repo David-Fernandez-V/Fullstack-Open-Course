53605cec4682fed615933f2794cccd8331d465ca
David-Fernandez-V <david.fernandez.v04@gmail.com>
2026-03-17 12:38:26 -0600

    Exercise 4.11

--- part4/bloglist_backend/tests/blog_api.test.js
+++ part4/bloglist_backend/tests/blog_api.test.js
@@ -59,6 +59,18 @@ test("A valid blog can be added", async () => {
   assert(addedBlog);
 });
 
+test("If likes property is missing will have the value of 0", async () => {
+  const newBlog = {
+    title: "Deep Dive Into Modern Web Development",
+    author: "Matti Luukkainen",
+    url: "https://fullstackopen.com/en/",
+  };
+
+  const response = await api.post("/api/blogs").send(newBlog);
+
+  assert.strictEqual(response.body.likes, 0);
+});
+
 after(async () => {
   await mongoose.connection.close();
 });
